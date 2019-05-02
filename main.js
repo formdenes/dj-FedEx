87c4d3c40cc82d75c05fff46f0994f9864625da2
Formanek Denes <formdenes@gmail.com>
2019-05-02 14:02:36 +0200

    MINDEN

--- main.js
+++ main.js
@@ -1,21 +1,62 @@
-let x_vals = [];
+function setup() {
+  createCanvas(256, 256);
+  const pixelModel = tf.sequential();
+
+const hidden = tf.layers.dense({
+  units: 4,
+  inputShape: [2],
+  activation: 'sigmoid',
+});
+
+const output = tf.layers.dense({
+  units: 1,
+  activation: 'sigmoid',
+});
+
+pixelModel.add(hidden);
+pixelModel.add(output);
+
+pixelModel.summary();
+
+const makeInputs = (width, height) => {
+  let coordinates = [];
+  for(let i = 0; i < width; i++){
+    for(let j = 0; j < height; j++){
+      coordinates.push([i,j])
+    }
+  }
+  return coordinates;
+}
+
+const input = tf.tensor2d(
+  makeInputs(256,256)
+)
+
+const outputs = pixelModel.predict(input);
+tf.print(outputs);
+
+let pixelValues;
+
+outputs.data().then(data => {
+  pixelValues = data.map(val => map(val,0,1,0,255));
+  console.log('Greyscale values',pixelValues);
+});
+}
+
+
+
+
+/* let x_vals = [];
 let y_vals = [];
 
 let a, b, c, d;
 let dragging = false;
 
 const learningRate = 0.2;
-const optimizer = tf.train.adam(learningRate);
+const optimizer = tf.train.adam(learningRate); */
 
-function setup() {
-  createCanvas(400, 400);
-  a = tf.variable(tf.scalar(random(-1, 1)));
-  b = tf.variable(tf.scalar(random(-1, 1)));
-  c = tf.variable(tf.scalar(random(-1, 1)));
-  d = tf.variable(tf.scalar(random(-1, 1)));
-}
 
-function loss(pred, labels) {
+/* function loss(pred, labels) {
   return pred.sub(labels).square().mean();
 }
 
@@ -27,19 +68,19 @@ function predict(x) {
     .add(xs.mul(c))
     .add(d);
   return ys;
-}
+} */
 
 
-function mousePressed() {
+/* function mousePressed() {
   dragging = true;
 }
 
 function mouseReleased() {
   dragging = false;
-}
+} */
 
 function draw() {
-  if (dragging) {
+  /* if (dragging) {
     let x = map(mouseX, 0, width, -1, 1);
     let y = map(mouseY, 0, height, 1, -1);
     x_vals.push(x);
@@ -82,5 +123,5 @@ function draw() {
     let y = map(curveY[i], -1, 1, height, 0);
     vertex(x, y);
   }
-  endShape();
+  endShape(); */
 }
